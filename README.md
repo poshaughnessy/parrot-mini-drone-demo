a4c9a5a3b558674fc82ce08f4f050508baf8b1ee
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2016-01-30 17:48:01 +0000

    Got it working

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # Parrot Mini Drone Demo
 
-Controlling a Parrot Mini Drone using voodootikigod's node-rolling-spider library.
+Controlling a Parrot Mini Drone using [node-rolling-spider](https://github.com/voodootikigod/node-rolling-spider).
+
+## Instructions
+
+Prerequisites:
+
+* First time: `npm install`
+* Ensure Bluetooth is switched on on your laptop.
+* Ensure the drone is switched on and the lights are green.
+
+Then:
+
+`npm start`.
 
--- index.js
+++ index.js
@@ -3,10 +3,12 @@
 const RollingSpider = require('rolling-spider');
 const temporal = require('temporal');
 
-let drone = new RollingSpider();
+let drone = new RollingSpider({logger: console.log});
 
-
+console.log('Connect...');
 drone.connect(function() {
+
+  console.log('Setup...');
   drone.setup(function() {
 
     // Reset the trim so the drone's flight is stable - "should always be called before taking off"
@@ -15,27 +17,28 @@ drone.connect(function() {
     // Starts sending the current speed values to the drone every 50 milliseconds
     drone.startPing();
 
-    // TODO do we need this again?
     drone.calibrate();
 
     temporal.queue([
       {
         delay: 5000,
         task: function () {
+          console.log('Take off...');
           drone.takeOff();
-          // TODO do we need this again?
           drone.calibrate();
         }
       },
       {
         delay: 5000,
         task: function () {
+          console.log('Land...');
           drone.land();
         }
       },
       {
         delay: 5000,
         task: function () {
+          console.log('Goodbye.');
           temporal.clear();
           process.exit(0);
         }
